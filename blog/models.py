b1fca440a59ac4b53ad647c37196d071e9731fdb
smithl <smith@imsweb.com>
2017-07-24 13:05:06 -0400

    plzz work

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,8 @@ class Post(models.Model):
         
     def __str__(self):
         return self.title
+    
+    
     def approved_comments(self):
         return self.comments.filter(approved_comment=True)
     
